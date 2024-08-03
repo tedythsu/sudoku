8d7cb78e47e468d169333729d8f754b1da1617c2
Ted <ted.yt.hsu@gmail.com>
2024-08-03 23:15:25 +0800

    feat: update number selection function

--- src/Board.js
+++ src/Board.js
@@ -3,6 +3,7 @@ import './Board.css';
 
 const Board = () => {
   const [board, setBoard] = useState(null);
+  const [selectedCell, setSelectedCell] = useState(null);
   const renderAfterCalled = useRef(false);
 
   useEffect(() => {
@@ -20,6 +21,25 @@ const Board = () => {
     renderAfterCalled.current = true;
   }, []);
 
+  const handleCellClick = (param1, param2) => {
+    setSelectedCell({ row: param1, col: param2 });
+    console.log(selectedCell)
+  };
+
+  const selectNumber = (number) => {
+    const answer = board.newboard.grids[0].solution[selectedCell.row][selectedCell.col];
+
+    if (number === answer) {
+      const updatedBoard = JSON.parse(JSON.stringify(board));
+      updatedBoard.newboard.grids[0].value[selectedCell.row][selectedCell.col] = number;
+      setBoard(updatedBoard);
+    } else {
+      window.alert('錯誤!')
+    }
+  }
+
+  const numbers = Array.from({ length: 9 }, (_, index) => index + 1);
+
   if (!board) {
     return <div>Loading...</div>;
   }
@@ -29,13 +49,25 @@ const Board = () => {
       <div>Difficulty: {board.newboard.grids[0].difficulty}</div>
       <div className='board'>
         {board.newboard.grids[0].value.map((row, rowIndex) => (
-          <div key={rowIndex} className='row'>
+          <div 
+            key={rowIndex}
+            className={`row ${selectedCell && selectedCell.row === rowIndex ? 'row--selected' : ''}`.trim()}
+          >
             {row.map((cell, cellIndex) => (
-              <div key={cellIndex} className='cell'>
+              <div 
+                key={cellIndex} 
+                className={`cell ${selectedCell && selectedCell.row === rowIndex && selectedCell.col === cellIndex ? 'cell--selected' : ''} ${cell === 0 ? 'clickable-cell' : ''} ${selectedCell && selectedCell.col === cellIndex ? 'col--selected' : ''}`.trim()}  
+                onClick={() => handleCellClick(rowIndex, cellIndex)}
+              >
                 {cell !== 0 ? cell : ''}
               </div>
             ))}
           </div>
+        ))}
+      </div>
+      <div className='number-list'>
+        {numbers.map((number) => (
+          <div className='number-list__item' key={number} onClick={() => selectNumber(number)}>{number}</div>
         ))}
       </div>
     </div>
